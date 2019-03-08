1c2b7c33bcd202c694b0ff40d069bec5fe7edb9a
Ricardo Andrade <ricardo@andrade.com>
2019-03-08 17:46:04 -0300

    enhance plots by controlling series colors

--- csv_read.m
+++ csv_read.m
@@ -11,19 +11,33 @@ Refs = CSV(:, 8:11);
 T = CSV(:, 12);
 
 %% Plots
+set(0,'defaulttextInterpreter','latex')
 
 figure(1)
-plot(t, Pos)
+posPlot = plot(t, Pos(:,1),'r', t, Pos(:,2), 'g', t, Pos(:,3), 'b')
 legend('X','Y','Z')
+xlabel('Time [s]');
+ylabel('Position [m]');
+% axObjs = posPlot.Children
+% dataObjs = axObjs.Children
+
+% posChild = posPlot.Children;
+% axes = gca;
 
 figure(2)
 subplot(2, 1, 1)
-plot(t, Euler)
-legend('Theta','Phi','Psi')
+attiPlot = plot(t, Euler)
+attiPlot(1).Color = 'r'; attiPlot(2).Color = 'g'; attiPlot(3).Color = 'b';
+
+legend('\theta','\phi','\psi')
 subplot(2, 1, 2)
-plot(t, Refs)
-legend('theta_r','phi_r','psi_r', 'Z_ref')
+attiRefPlot = plot(t, Refs)
+attiRefPlot(1).Color = 'r'; attiRefPlot(2).Color = 'g'; attiRefPlot(3).Color = 'b';
+legend('\theta_r','\phi_r','\psi_r', 'Z_r')
 
 
 figure(3)
-plot(t, [T Pos(:,3) Refs(:,4)])
+controlPlot = plot(t, [T Pos(:,3) Refs(:,4)])
+controlPlot(1).Color = 'm'; controlPlot(2).Color = 'b'; controlPlot(3).Color = 'k';
+controlPlot(3).LineStyle = '--';
+legend('Thrust','Z','Z_r','--')
